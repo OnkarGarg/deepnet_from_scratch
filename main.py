66b863a9682dd8bca06c6d6a32993bc50ab3e8e5
OnkarGarg <onkargarg6@gmail.com>
2025-06-24 21:23:31 +0100

    Currently testing DenseLayer class

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Layers.DenseLayer import DenseLayer
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = DenseLayer(10, 4)
+print(a)
+print("Output:", a.output("RELU"))
